822662309ad520931f6d6053c0a9c07e1cb1dcb6
Sharon Abishek <sharon@Sharons-MacBook-Air.local>
2021-01-18 11:41:34 +0530

    added json

--- app.py
+++ app.py
@@ -6,6 +6,32 @@ app = Flask(__name__)
 def login():
     return render_template("index.html")
 
+@app.route('/result', methods = ['GET', 'POST'])
+def result():
+    song_name = request.form.get('song')
+    print(song_name)
+
+    #if (song_name = song_details.song_name):
+
+    
+
+    song_details = [
+        {
+            "song_name" : "Vaathi Raid",
+            "movie_name": "Master",
+            "music_director" : "Anirudh",
+            "singer_name" : "Arivu"
+        },
+        {
+            "song_name" : "Chellama",
+            "movie_name": "Doctor",
+            "music_director" : "Anirudh",
+            "singer_name" : "Anirudh"
+        }
+    ]
+
+    return render_template("index.html", song_details=song_details,song_name=song_name)
+   
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
